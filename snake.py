0c7b9ee30dd3d00a6209e79cb83619e32068aa63
Administrator <799691667@qq.com>
2026-07-24 17:58:55 +0800

    改变食物颜色

--- snake.py
+++ snake.py
@@ -23,7 +23,7 @@ FAST_FPS: int = 20
 # 颜色
 BG_COLOR        = (24, 28, 32)       # 深色背景
 GRID_COLOR      = (34, 38, 42)       # 网格线
-FOOD_COLOR      = (100, 85, 85)      # 食物红色
+FOOD_COLOR      = (0, 85, 85)      # 食物红色
 HEAD_COLOR      = (80, 220, 80)      # 蛇头绿色
 BODY_COLOR      = (200, 60, 60)      # 蛇身红色
 EYE_WHITE_COLOR = (255, 255, 255)    # 眼白
